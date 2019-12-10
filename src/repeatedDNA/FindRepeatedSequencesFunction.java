84efd98503b5093d878dfd0b3959386c9bb02989
chaninkou <michaelkoulove@yahoo.com>
2019-12-09 22:43:49 -0800

    Great question on finding patterns with HashMap, runtime not that great.

---
+++ src/repeatedDNA/FindRepeatedSequencesFunction.java
@@ -0,0 +1,29 @@
+package repeatedDNA;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindRepeatedSequencesFunction {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        int i = 0;
+        
+        while(i + 10 <= s.length()){
+            String sequences = s.substring(i, i + 10);
+            i++;
+            
+            map.put(sequences, map.getOrDefault(sequences, 0) + 1);
+            
+            if(map.get(sequences) == 2){
+                result.add(sequences);
+            }
+        }
+              
+        return result;
+    }
+}
---
+++ src/repeatedDNA/Main.java
@@ -0,0 +1,13 @@
+package repeatedDNA;
+
+public class Main {
+	public static void main(String[] args) {
+		String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+		
+		System.out.println("Input: " + s);
+		
+		FindRepeatedSequencesFunction solution = new FindRepeatedSequencesFunction();
+		
+		System.out.println("Solution: " + solution.findRepeatedDnaSequences(s));
+	}
+}
